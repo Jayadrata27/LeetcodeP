c0ac7e6d3dc009558f25c48a1c60de2071e25aa2
Jayadrata Roy <royjayadrata@outlook.com>
2026-09-09 20:35:02 +0530

    Time: 3 ms (64.54%) | Memory: 7.9 MB (21.34%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<2){
+            return n;
+        }
+        int first=0,second=1,curr;
+        for(int i=2;i<=n;i++){
+            curr=first+second;
+            first=second;
+            second=curr;
+        }
+        return curr;
+    }
+};
